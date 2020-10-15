a60c6cfe629c75ad7c789a1acd597c0634ef942a
aurxine <sarowardhrubo@gmail.com>
2020-10-15 10:02:14 +0600

    Energy_Meter code uploaded.
    Other classes will be added later

---
+++ Energy_Meter.py
@@ -0,0 +1,383 @@
+from pymodbus.client.sync import ModbusSerialClient
+
+def decode(lst):
+    s = '0x'
+    for x in lst:
+        x = hex(x)
+        s += x[2:]
+    return int(s,0)
+
+
+class EnergyMeter_DZS100():
+    
+    def __init__(self, method='rtu', port='/dev/ttyUSB0', baudrate=9600, timeout=3, parity='E', stopbits=1, bytesize=8):
+        self.method = method
+        self.port = port
+        self.baudrate = baudrate
+        self.timeout = timeout
+        self.parity = parity
+        self.stopbits = stopbits
+        self.bytesize = bytesize
+    
+    def readCombinedActiveEnergy(self):
+     
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=0, count=2, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readImportActiveEnergy(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=2, count=2, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readExportActiveEnergy(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=4, count=2, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readVoltage(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=6, count=1, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readCurrent(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=7, count=2, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readActivePower(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=9, count=2, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readPowerFactor(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=11, count=1, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readFrequency(self):
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        )
+        
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=12, count=1, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+            
+    
+# dsz100 = EnergyMeter_DZS100(port='COM10', baudrate=2400)
+
+class EnergyMeter_DZS500():
+    
+    def __init__(self, method='rtu', port='/dev/ttyUSB0', baudrate=9600, timeout=3, parity='E', stopbits=1, bytesize=8):
+        self.method = method
+        self.port = port
+        self.baudrate = baudrate
+        self.timeout = timeout
+        self.parity = parity
+        self.stopbits = stopbits
+        self.bytesize = bytesize
+        
+    def readCurrent(self, phase = None):
+        
+        addr = None        
+        if phase == None:
+            print("Please call with phase")
+            return
+        
+        if phase == 'A':
+           addr = 16
+        elif phase == 'B':
+            addr = 17
+        elif phase == 'C':
+            addr = 18
+        else:
+            print("Please enter correct phase")
+            return
+       
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        ) 
+                
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=addr, count=1, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+    def readVoltage(self, phase=None, line=None):
+        
+        addr = None        
+        if phase == 'A':
+            addr = 20
+        elif phase == 'B':
+            addr = 21
+        elif phase == 'C':
+            addr = 22
+        elif line == 'AB':
+            addr = 23
+        elif line == 'BC':
+            addr = 24
+        elif line == 'CA':
+            addr = 25
+        else:
+            print("Please enter valid input")
+            return
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        ) 
+                
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=addr, count=1, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+            
+    def readPower(self, category, phase=None):
+        
+        addr = None
+        if phase == None:
+            if category == 'active':
+                addr = 26
+            elif category == 'reactive':
+                addr = 27
+            elif category == 'apparent':
+                addr = 28
+            elif category == 'factor':
+                addr = 29
+                
+        elif phase == 'A':
+            if category == 'active':
+                addr = 31
+            elif category == 'reactive':
+                addr = 34
+            elif category == 'apparent':
+                addr = 37
+            elif category == 'factor':
+                addr = 40
+                
+        elif phase == 'B':
+            if category == 'active':
+                addr = 32
+            elif category == 'reactive':
+                addr = 35
+            elif category == 'apparent':
+                addr = 38
+            elif category == 'factor':
+                addr = 41
+                
+        elif phase == 'C':
+            if category == 'active':
+                addr = 33
+            elif category == 'reactive':
+                addr = 36
+            elif category == 'apparent':
+                addr = 39
+            elif category == 'factor':
+                addr = 42
+        if addr == None:
+            print("Invalid input")
+            return
+        
+        client = ModbusSerialClient(
+            method = self.method,
+            port = self.port, #'/dev/ttyUSB0', #'COM9',
+            baudrate = self.baudrate,
+            timeout = self.timeout,
+            parity = self.parity,
+            stopbits = self.stopbits,
+            bytesize = self.bytesize
+        ) 
+                
+        if client.connect():
+            print("Connected to the Modbus Server/Slave")
+            # Reading from a holding register with the below content.
+            res = client.read_holding_registers(address=addr, count=1, unit=5)
+            
+            if not res.isError():
+                print(decode(res.registers))
+            else:
+                print(res)
+        else:
+            print('Cannot connect to the Modbus Server/Slave')
+    
+        
+dzs500 = EnergyMeter_DZS500( port='COM10', baudrate=2400)
+
+dzs100 = EnergyMeter_DZS100(port= 'COM10', baudrate= 2400)
+        
+            
+            
+        
