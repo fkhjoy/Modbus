9d74352533f6c6974093c7f7fb05693d91b2aff8
aurxine <sarowardhrubo@gmail.com>
2020-10-18 15:49:32 +0600

    added polyfit feature

--- Level_Transmitter.py
+++ Level_Transmitter.py
@@ -1,5 +1,6 @@
 import pymodbus
 from pymodbus.client.sync import ModbusSerialClient
+import numpy as np
 
 def decode(lst):
     s = '0x'
@@ -9,7 +10,8 @@ def decode(lst):
     return int(s,0)
 
 class AR6451():
-    def __init__(self, method='rtu', port='COM9', baudrate=9600, timeout=3, parity='N', stopbits=1, bytesize=8, slaveAddress = 1):
+    def __init__(self, method='rtu', port='COM9', baudrate=9600, timeout=3, parity='N', stopbits=1, 
+    bytesize=8, slaveAddress = 1, m = 107.143, b = -91.36):
         self.method = method
         self.port = port
         self.baudrate = baudrate
@@ -18,6 +20,8 @@ class AR6451():
         self.stopbits = stopbits
         self.bytesize = bytesize
         self.slaveAddress = slaveAddress
+        self.m = m
+        self.b = b
         self.client = ModbusSerialClient(
             method = self.method,
             port = self.port,
@@ -39,24 +43,28 @@ class AR6451():
                 print(response)
         else:
             print("Cannot connect to slave")
+    
+    def fitLine(self, Points):
+        x = Points[0]
+        y = Points[1]
+        self.m, self.b = np.polyfit(x, y, 1)
 
     def Pressure(self, Print = True):
         pressure = self.readRegister(address = 4, Print = Print)
         dot_point = self.readRegister(address= 3, Print = Print)
         return pressure/(10**dot_point)
     
-    def Water_Level(self, Print = True, unit = 'cm', m = 107.143, c = -91.36):
+    def Water_Level(self, unit = 'cm', Print = True):
         if unit == 'cm':
             pressure = self.Pressure(Print = Print)
-            return m*pressure +c
+            return self.m*pressure + self.b
         if unit == 'm':
             pressure = self.Pressure(Print = Print)
-            return (m*pressure +c)/100
+            return (self.m*pressure + self.b)/100
         if unit == 'in':
             pressure = self.Pressure(Print = Print)
-            return (m*pressure +c)*2.54
+            return (self.m*pressure + self.b)*2.54
 
 level_sensor = AR6451()
 
 p = level_sensor.Pressure(Print = False)
-print(p)
