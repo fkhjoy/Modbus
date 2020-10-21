276b7d88720c0950af031335475706dd7f8cf6ef
aurxine <sarowardhrubo@gmail.com>
2020-10-21 11:32:48 +0600

    AMR needs GPIO

--- DWASA_SCADA.py
+++ DWASA_SCADA.py
@@ -1,6 +1,6 @@
 '''
 This code will run in Rasspberry Pi. The code will do the following,
-    1. Retrieve data from VFD, Level Transmitter and Energy meter
+    1. Retrieve data from VFD, Level Transmitter, Energy meter and AMR.
     2. Format the acquired data as json and send it to the broker with a unique topic
     3. Subscribe to that unique topic and receive commands
     4. Take actions according to those commands
