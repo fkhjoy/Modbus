3b1f206f9164c06a6e2066b075e5e6ec85af84d8
aurxine <sarowardhrubo@gmail.com>
2020-11-10 13:01:00 +0600

    changed m

--- Level_Transmitter.py
+++ Level_Transmitter.py
@@ -10,7 +10,7 @@ def decode(lst):
     return int(s,0)
 
 class AR6451():
-    def __init__(self, client, slaveAddress = 1, m = 0.00010204082, b = 0):
+    def __init__(self, client, slaveAddress = 1, m = 102.04081632653, b = 0):
         
         self.slaveAddress = slaveAddress
         self.m = m
